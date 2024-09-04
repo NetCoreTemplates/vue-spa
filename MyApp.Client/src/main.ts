55fe403f6fbe68c77d71efb596d7b4fcbc53ffec
Demis Bellot <demis.bellot@gmail.com>
2024-09-04 21:43:29 +0800

    upgrade to latest vue deps

--- MyApp.Client/src/main.ts
+++ MyApp.Client/src/main.ts
@@ -7,6 +7,7 @@ import App from './App.vue'
 
 import ServiceStackVue from "@servicestack/vue"
 import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { createPinia } from "pinia"
 import press from "virtual:press"
@@ -18,6 +19,16 @@ import { client } from "@/api"
 
 const app = createApp(App)
 const head = createHead()
+
+routes.forEach((route:any) => {
+    // Force all pages in /admin to use /layout/admin.vue
+    if (route.path.startsWith('/admin')) {
+        (route.children ?? []).forEach((child:any) => {
+            child.meta ??= {}
+            child.meta.layout = 'admin'
+        })
+    }
+})
 
 export const router = configRouter(createRouter({
     history: createWebHistory(),
@@ -34,18 +45,7 @@ export const router = configRouter(createRouter({
             }, 1)
         }
     },
-    extendRoutes(routes:any) {
-        routes.forEach((route:any) => {
-            // Force all pages in /admin to use /layout/admin.vue
-            if (route.path.startsWith('/admin')) {
-                (route.children ?? []).forEach((child:any) => {
-                    child.meta ??= {}
-                    child.meta.layout = 'admin'
-                })
-            }
-        })
-        return setupLayouts(routes)
-    },
+    routes: setupLayouts(routes)
 }))
 // handle external links
 router.beforeEach((to, _from, next) => {
