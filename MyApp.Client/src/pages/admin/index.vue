47d3f3522cca781afdf6b297a78ad68656d99e5c
Demis Bellot <demis.bellot@gmail.com>
2025-12-06 13:40:07 +0800

    update dtos

--- MyApp.Client/src/pages/admin/index.vue
+++ MyApp.Client/src/pages/admin/index.vue
@@ -1,12 +1,5 @@
 <template>
   <div>
-    <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-      <RouterLink v-for="stat in stats" :key="stat.label" :to="`/admin/${stat.label.toLowerCase()}`"
-           class="block cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-900 overflow-hidden rounded-lg bg-white dark:bg-black px-4 py-5 shadow sm:p-6 dark:border-2 dark:border-pink-600 dark:hover:border-blue-600">
-        <dt class="truncate text-sm font-medium text-gray-500">Total {{humanize(stat.label)}}</dt>
-        <dd class="mt-1 text-3xl font-semibold tracking-tight text-gray-900 dark:text-gray-50">{{formatNumber(stat.total)}}</dd>
-      </RouterLink>
-    </dl>
     <p class="mt-8 text-sm font-semibold leading-6">
       Go to <RouterLink to="/" class="text-indigo-600">Home <span aria-hidden="true">&rarr;</span></RouterLink>
     </p>
@@ -29,15 +22,3 @@
     <SrcPage path="admin/index.vue" />
   </div>
 </template>
-
-<script setup lang="ts">
-import { ref } from "vue"
-import { humanize, type ApiResult } from "@servicestack/client"
-import { useClient } from "@servicestack/vue"
-import { AdminData, type AdminDataResponse, type PageStats } from "@/dtos"
-
-const client = useClient()
-const stats = ref<PageStats[]>([])
-client.swr(new AdminData(), (r:ApiResult<AdminDataResponse>) => stats.value = r.response?.pageStats || [])
-const formatNumber = (value:number) => new Intl.NumberFormat().format(value)
-</script>
